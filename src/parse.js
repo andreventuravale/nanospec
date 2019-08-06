4cfebc32da7d565a49fb5c5dad4df612e05f6343
andreventuravale <andreventuravale@gmail.com>
2019-08-06 20:44:18 -0300

    refactoring and improving grammar

--- src/parse.js
+++ src/parse.js
@@ -5,41 +5,104 @@ const {
   token
 } = require('nanogram')
 
-const COLON = token('colon', /:/)
-const WS = token('ws', /\s+/m)
+global.inspect = x => console.log(require('util').inspect(x, false, 100, true))
+
+const COLON = token(Symbol.for('colon'), /:/)
 const FEATURE = token('feature', /feature/i)
-const PHRASE = token(Symbol.for('phrase'), /[^\r\n]+/i)
+const PHRASE = token(Symbol.for('phrase'), /(?!(?:given|when|then|and|but)[ \t])[^\s]+([ \t]+[^\s]+)*/yi)
+const SCENARIO = token(Symbol.for('scenario'), /scenario/i)
+const STEP = token(Symbol.for('phrase'), /(?:(given|when|then|and|but)[ \t]+)([^\r\n]+)/i)
+const WS = token('ws', /\s+/ym)
 
 const text = list(Symbol.for('text'), PHRASE, WS)
+
+const steps = list(Symbol.for('steps'), STEP, WS)
 
 const feature = compose('feature',
   WS,
   FEATURE,
-  WS,
+  optional(WS),
   COLON,
   WS,
   PHRASE,
   WS,
-  optional(
-    text
-  )
+  optional(text)
 )
+
+const scenario = compose('scenario',
+  optional(WS),
+  SCENARIO,
+  optional(WS),
+  COLON,
+  optional(WS),
+  PHRASE,
+  optional(WS),
+  /* TODO GROUP
+  [choose|group|or|single](
+    optional(text),
+    optional(steps)
+  )
+  */
+  optional(text),
+  optional(steps)
+)
+
+function transformScenario ($scenario) {
+  return {
+    type: 'statement',
+    subtype: 'scenario',
+    nodes: [
+      {
+        type: 'title',
+        text: $scenario[Symbol.for('phrase')][0].data[0]
+      },
+      ...$scenario[Symbol.for('text')][0].data.map(i => ({
+        type: 'summary',
+        text: i.data[0]
+      })),
+      ...$scenario[Symbol.for('steps')][0].data.map(i => ({
+        type: 'step',
+        subtype: i.data[1].toLowerCase(),
+        nodes: [
+          { type: 'definition', text: i.data[2] },
+          { type: 'token', subtype: 'keyword', text: i.data[1] }
+        ]
+      })),
+      {
+        subtype: 'keyword',
+        text: $scenario[Symbol.for('scenario')][0].data[0],
+        type: 'token'
+      },
+      {
+        subtype: 'colon',
+        text: $scenario[Symbol.for('colon')][0].data[0],
+        type: 'token'
+      }
+    ]
+  }
+}
 
 function parse (input) {
   let offset = 0
 
   let $feature = feature(input, offset)
 
-  let result = {
-    type: 'feature',
-    title: $feature[Symbol.for('phrase')][0].data[0],
-    summary: $feature[Symbol.for('text')][0].data.map(i => ({
-      type: 'text',
-      text: i.data[0]
-    }))
+  if ($feature.found) {
+    return {
+      type: 'feature',
+      title: $feature[Symbol.for('phrase')][0].data[0],
+      summary: $feature[Symbol.for('text')][0].data.map(i => ({
+        type: 'text',
+        text: i.data[0]
+      }))
+    }
   }
 
-  return result
+  let $scenario = scenario(input, offset)
+
+  if ($scenario.found) {
+    return transformScenario($scenario)
+  }
 }
 
 module.exports = {
--- test/parser/scenario.test.js
+++ test/parser/scenario.test.js
@@ -1,7 +1,7 @@
 const { expect } = require('chai')
 const { parse } = require('./util')
 
-suite('Parser / Scenario', () => {
+suite.only('Parser / Scenario', () => {
   test('Parses a basic scenario', () => {
     const metadata = parse(`
 
@@ -15,11 +15,34 @@ suite('Parser / Scenario', () => {
     expect(metadata).to.deep.eql({
       type: 'statement',
       subtype: 'scenario',
-      title: 'Quis autem vel eum',
       nodes: [
-        { type: 'step', subtype: 'given', text: 'taque earum rerum', fullText: 'Given taque earum rerum' },
-        { type: 'step', subtype: 'when', text: 'minus id quod maxime', fullText: 'When minus id quod maxime' },
-        { type: 'step', subtype: 'then', text: 'et expedita distinctio', fullText: 'Then et expedita distinctio' }
+        { type: 'title', text: 'Quis autem vel eum' },
+        {
+          type: 'step',
+          subtype: 'given',
+          nodes: [
+            { type: 'definition', text: 'taque earum rerum' },
+            { type: 'token', subtype: 'keyword', text: 'Given' }
+          ]
+        },
+        {
+          type: 'step',
+          subtype: 'when',
+          nodes: [
+            { type: 'definition', text: 'minus id quod maxime' },
+            { type: 'token', subtype: 'keyword', text: 'When' }
+          ]
+        },
+        {
+          type: 'step',
+          subtype: 'then',
+          nodes: [
+            { type: 'definition', text: 'et expedita distinctio' },
+            { type: 'token', subtype: 'keyword', text: 'Then' }
+          ]
+        },
+        { type: 'token', subtype: 'keyword', text: 'Scenario' },
+        { type: 'token', subtype: 'colon', text: ':' }
       ]
     })
   })
@@ -40,15 +63,108 @@ suite('Parser / Scenario', () => {
     expect(metadata).to.deep.eql({
       type: 'statement',
       subtype: 'scenario',
-      title: 'Quis autem vel eum',
-      summary: [
-        { type: 'text', text: 'Qui occaecat excepteur sit minim dolor' },
-        { type: 'text', text: 'ipsum pariatur cupidatat officia aute nisi.' }
-      ],
       nodes: [
-        { type: 'step', subtype: 'given', text: 'taque earum rerum', fullText: 'Given taque earum rerum' },
-        { type: 'step', subtype: 'when', text: 'minus id quod maxime', fullText: 'When minus id quod maxime' },
-        { type: 'step', subtype: 'then', text: 'et expedita distinctio', fullText: 'Then et expedita distinctio' }
+        { type: 'title', text: 'Quis autem vel eum' },
+        { type: 'summary', text: 'Qui occaecat excepteur sit minim dolor' },
+        { type: 'summary', text: 'ipsum pariatur cupidatat officia aute nisi.' },
+        {
+          type: 'step',
+          subtype: 'given',
+          nodes: [
+            { type: 'definition', text: 'taque earum rerum' },
+            { type: 'token', subtype: 'keyword', text: 'Given' }
+          ]
+        },
+        {
+          type: 'step',
+          subtype: 'when',
+          nodes: [
+            { type: 'definition', text: 'minus id quod maxime' },
+            { type: 'token', subtype: 'keyword', text: 'When' }
+          ]
+        },
+        {
+          type: 'step',
+          subtype: 'then',
+          nodes: [
+            { type: 'definition', text: 'et expedita distinctio' },
+            { type: 'token', subtype: 'keyword', text: 'Then' }
+          ]
+        },
+        { type: 'token', subtype: 'keyword', text: 'Scenario' },
+        { type: 'token', subtype: 'colon', text: ':' }
+      ]
+    })
+  })
+
+  test('The keyword is not case sensitive', () => {
+    const metadata = parse(`
+      SCENario : Amet eiusmod ipsum voluptate dolore incididunt.
+    `)
+
+    expect(metadata).to.deep.eql({
+      type: 'statement',
+      subtype: 'scenario',
+      nodes: [
+        { type: 'title', text: 'Amet eiusmod ipsum voluptate dolore incididunt.' },
+        { type: 'token', subtype: 'keyword', text: 'SCENario' },
+        { type: 'token', subtype: 'colon', text: ':' }
+      ]
+    })
+  })
+
+  test('There is no need of a space between the keyword and the colon', () => {
+    const metadata = parse(` Scenario: Qui aliqua elit mollit non irure incididunt veniam ea laborum. `)
+
+    expect(metadata).to.deep.eql({
+      type: 'statement',
+      subtype: 'scenario',
+      nodes: [
+        { type: 'title', text: 'Qui aliqua elit mollit non irure incididunt veniam ea laborum.' },
+        { type: 'token', subtype: 'keyword', text: 'Scenario' },
+        { type: 'token', subtype: 'colon', text: ':' }
+      ]
+    })
+  })
+
+  test('There is no need of a space in the begin of the input', () => {
+    const metadata = parse(`Scenario: Qui aliqua elit mollit non irure incididunt veniam ea laborum. `)
+
+    expect(metadata).to.deep.eql({
+      type: 'statement',
+      subtype: 'scenario',
+      nodes: [
+        { type: 'title', text: 'Qui aliqua elit mollit non irure incididunt veniam ea laborum.' },
+        { type: 'token', subtype: 'keyword', text: 'Scenario' },
+        { type: 'token', subtype: 'colon', text: ':' }
+      ]
+    })
+  })
+
+  test('There is no need of a space in the end of the input', () => {
+    const metadata = parse(` Scenario: Qui aliqua elit mollit non irure incididunt veniam ea laborum.`)
+
+    expect(metadata).to.deep.eql({
+      type: 'statement',
+      subtype: 'scenario',
+      nodes: [
+        { type: 'title', text: 'Qui aliqua elit mollit non irure incididunt veniam ea laborum.' },
+        { type: 'token', subtype: 'keyword', text: 'Scenario' },
+        { type: 'token', subtype: 'colon', text: ':' }
+      ]
+    })
+  })
+
+  test('There is no need of a space between the colon and the title', () => {
+    const metadata = parse(` Scenario :Qui aliqua elit mollit non irure incididunt veniam ea laborum. `)
+
+    expect(metadata).to.deep.eql({
+      type: 'statement',
+      subtype: 'scenario',
+      nodes: [
+        { type: 'title', text: 'Qui aliqua elit mollit non irure incididunt veniam ea laborum.' },
+        { type: 'token', subtype: 'keyword', text: 'Scenario' },
+        { type: 'token', subtype: 'colon', text: ':' }
       ]
     })
   })
