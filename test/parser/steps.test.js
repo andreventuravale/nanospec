f1369f534a92d694602c3c04ca089ae472b06835
andreventuravale <andreventuravale@gmail.com>
2020-01-10 15:22:30 -0300

    Reactivating steps suite

--- test/parser/steps.test.js
+++ test/parser/steps.test.js
@@ -1,7 +1,7 @@
 const { expect } = require('chai')
 const { parse } = require('./util')
 
-suite('Parser / Steps', () => {
+suite.only('Parser / Steps', () => {
   test('Parses "given" steps with ands and a but', () => {
     const metadata = parse(`
 
@@ -16,16 +16,113 @@ suite('Parser / Steps', () => {
     `)
 
     expect(metadata).to.deep.eql({
-      type: 'statement',
-      subtype: 'scenario',
-      title: 'Ex nisi eu aute ea reprehenderit proident et voluptate adipisicing nostrud et in id',
-      nodes: [
-        { type: 'step', subtype: 'given', text: 'Tempor labore sit anim do anim', fullText: 'Given Tempor labore sit anim do anim' },
-        { type: 'step', subtype: 'and', text: 'Veniam ex amet pariatur Lorem eu sint', fullText: 'And Veniam ex amet pariatur Lorem eu sint' },
-        { type: 'step', subtype: 'and', text: 'Qui veniam id excepteur et non pariatur consequat occaecat mollit dolore sit mollit pariatur qui', fullText: 'And Qui veniam id excepteur et non pariatur consequat occaecat mollit dolore sit mollit pariatur qui' },
-        { type: 'step', subtype: 'but', text: 'Velit nostrud pariatur irure fugiat et occaecat excepteur voluptate commodo amet ullamco', fullText: 'But Velit nostrud pariatur irure fugiat et occaecat excepteur voluptate commodo amet ullamco' },
-        { type: 'step', subtype: 'when', text: 'Excepteur cupidatat laborum non est', fullText: 'When Excepteur cupidatat laborum non est' },
-        { type: 'step', subtype: 'then', text: 'Quis cillum esse incididunt magna exercitation ex consectetur duis sint aute fugiat exercitation elit', fullText: 'Then Quis cillum esse incididunt magna exercitation ex consectetur duis sint aute fugiat exercitation elit' }
+      'type': 'statement',
+      'subtype': 'scenario',
+      'nodes': [
+        {
+          'type': 'title',
+          'text': 'Ex nisi eu aute ea reprehenderit proident et voluptate adipisicing nostrud et in id'
+        },
+        {
+          'type': 'step',
+          'subtype': 'given',
+          'nodes': [
+            {
+              'type': 'definition',
+              'text': 'Tempor labore sit anim do anim'
+            },
+            {
+              'type': 'token',
+              'subtype': 'keyword',
+              'text': 'Given'
+            }
+          ]
+        },
+        {
+          'type': 'step',
+          'subtype': 'and',
+          'nodes': [
+            {
+              'type': 'definition',
+              'text': 'Veniam ex amet pariatur Lorem eu sint'
+            },
+            {
+              'type': 'token',
+              'subtype': 'keyword',
+              'text': 'And'
+            }
+          ]
+        },
+        {
+          'type': 'step',
+          'subtype': 'and',
+          'nodes': [
+            {
+              'type': 'definition',
+              'text': 'Qui veniam id excepteur et non pariatur consequat occaecat mollit dolore sit mollit pariatur qui'
+            },
+            {
+              'type': 'token',
+              'subtype': 'keyword',
+              'text': 'And'
+            }
+          ]
+        },
+        {
+          'type': 'step',
+          'subtype': 'but',
+          'nodes': [
+            {
+              'type': 'definition',
+              'text': 'Velit nostrud pariatur irure fugiat et occaecat excepteur voluptate commodo amet ullamco'
+            },
+            {
+              'type': 'token',
+              'subtype': 'keyword',
+              'text': 'But'
+            }
+          ]
+        },
+        {
+          'type': 'step',
+          'subtype': 'when',
+          'nodes': [
+            {
+              'type': 'definition',
+              'text': 'Excepteur cupidatat laborum non est'
+            },
+            {
+              'type': 'token',
+              'subtype': 'keyword',
+              'text': 'When'
+            }
+          ]
+        },
+        {
+          'type': 'step',
+          'subtype': 'then',
+          'nodes': [
+            {
+              'type': 'definition',
+              'text': 'Quis cillum esse incididunt magna exercitation ex consectetur duis sint aute fugiat exercitation elit'
+            },
+            {
+              'type': 'token',
+              'subtype': 'keyword',
+              'text': 'Then'
+            }
+          ]
+        },
+        {
+          'subtype': 'keyword',
+          'text': 'Scenario',
+          'type': 'token'
+        },
+        {
+          'subtype': 'colon',
+          'text': ':',
+          'type': 'token'
+        }
       ]
     })
   })
@@ -44,16 +141,113 @@ suite('Parser / Steps', () => {
     `)
 
     expect(metadata).to.deep.eql({
-      type: 'statement',
-      subtype: 'scenario',
-      title: 'Nulla proident id deserunt aute pariatur esse id minim non pariatur laboris',
-      nodes: [
-        { type: 'step', subtype: 'given', text: 'Minim exercitation sunt consequat cillum nostrud sit magna id', fullText: 'Given Minim exercitation sunt consequat cillum nostrud sit magna id' },
-        { type: 'step', subtype: 'when', text: 'Sit cillum laboris amet aute officia amet qui voluptate nisi', fullText: 'When Sit cillum laboris amet aute officia amet qui voluptate nisi' },
-        { type: 'step', subtype: 'and', text: 'Aliqua aliquip eu et anim Lorem tempor dolore incididunt aute anim aute minim', fullText: 'And Aliqua aliquip eu et anim Lorem tempor dolore incididunt aute anim aute minim' },
-        { type: 'step', subtype: 'and', text: 'Magna cupidatat do pariatur minim Lorem consectetur', fullText: 'And Magna cupidatat do pariatur minim Lorem consectetur' },
-        { type: 'step', subtype: 'but', text: 'Consectetur sit aliquip sit laboris magna irure ullamco anim in id', fullText: 'But Consectetur sit aliquip sit laboris magna irure ullamco anim in id' },
-        { type: 'step', subtype: 'then', text: 'Ea minim do exercitation deserunt quis amet nostrud et', fullText: 'Then Ea minim do exercitation deserunt quis amet nostrud et' }
+      'type': 'statement',
+      'subtype': 'scenario',
+      'nodes': [
+        {
+          'type': 'title',
+          'text': 'Nulla proident id deserunt aute pariatur esse id minim non pariatur laboris'
+        },
+        {
+          'type': 'step',
+          'subtype': 'given',
+          'nodes': [
+            {
+              'type': 'definition',
+              'text': 'Minim exercitation sunt consequat cillum nostrud sit magna id'
+            },
+            {
+              'type': 'token',
+              'subtype': 'keyword',
+              'text': 'Given'
+            }
+          ]
+        },
+        {
+          'type': 'step',
+          'subtype': 'when',
+          'nodes': [
+            {
+              'type': 'definition',
+              'text': 'Sit cillum laboris amet aute officia amet qui voluptate nisi'
+            },
+            {
+              'type': 'token',
+              'subtype': 'keyword',
+              'text': 'When'
+            }
+          ]
+        },
+        {
+          'type': 'step',
+          'subtype': 'and',
+          'nodes': [
+            {
+              'type': 'definition',
+              'text': 'Aliqua aliquip eu et anim Lorem tempor dolore incididunt aute anim aute minim'
+            },
+            {
+              'type': 'token',
+              'subtype': 'keyword',
+              'text': 'And'
+            }
+          ]
+        },
+        {
+          'type': 'step',
+          'subtype': 'and',
+          'nodes': [
+            {
+              'type': 'definition',
+              'text': 'Magna cupidatat do pariatur minim Lorem consectetur'
+            },
+            {
+              'type': 'token',
+              'subtype': 'keyword',
+              'text': 'And'
+            }
+          ]
+        },
+        {
+          'type': 'step',
+          'subtype': 'but',
+          'nodes': [
+            {
+              'type': 'definition',
+              'text': 'Consectetur sit aliquip sit laboris magna irure ullamco anim in id'
+            },
+            {
+              'type': 'token',
+              'subtype': 'keyword',
+              'text': 'But'
+            }
+          ]
+        },
+        {
+          'type': 'step',
+          'subtype': 'then',
+          'nodes': [
+            {
+              'type': 'definition',
+              'text': 'Ea minim do exercitation deserunt quis amet nostrud et'
+            },
+            {
+              'type': 'token',
+              'subtype': 'keyword',
+              'text': 'Then'
+            }
+          ]
+        },
+        {
+          'subtype': 'keyword',
+          'text': 'Scenario',
+          'type': 'token'
+        },
+        {
+          'subtype': 'colon',
+          'text': ':',
+          'type': 'token'
+        }
       ]
     })
   })
@@ -72,16 +266,113 @@ suite('Parser / Steps', () => {
     `)
 
     expect(metadata).to.deep.eql({
-      type: 'statement',
-      subtype: 'scenario',
-      title: 'Ad Lorem cillum cupidatat deserunt nulla Lorem officia dolore dolore irure officia ex aliquip',
-      nodes: [
-        { type: 'step', subtype: 'given', text: 'Cupidatat labore est ipsum nostrud', fullText: 'Given Cupidatat labore est ipsum nostrud' },
-        { type: 'step', subtype: 'when', text: 'Irure sint sit id laboris incididunt do voluptate', fullText: 'When Irure sint sit id laboris incididunt do voluptate' },
-        { type: 'step', subtype: 'then', text: 'Enim enim sit labore ut veniam', fullText: 'Then Enim enim sit labore ut veniam' },
-        { type: 'step', subtype: 'and', text: 'Do laboris ullamco nostrud dolore officia', fullText: 'And Do laboris ullamco nostrud dolore officia' },
-        { type: 'step', subtype: 'and', text: 'Non adipisicing id sunt do qui enim consequat officia mollit eu labore irure dolor', fullText: 'And Non adipisicing id sunt do qui enim consequat officia mollit eu labore irure dolor' },
-        { type: 'step', subtype: 'but', text: 'Do dolore amet in ut adipisicing proident', fullText: 'But Do dolore amet in ut adipisicing proident' }
+      'type': 'statement',
+      'subtype': 'scenario',
+      'nodes': [
+        {
+          'type': 'title',
+          'text': 'Ad Lorem cillum cupidatat deserunt nulla Lorem officia dolore dolore irure officia ex aliquip'
+        },
+        {
+          'type': 'step',
+          'subtype': 'given',
+          'nodes': [
+            {
+              'type': 'definition',
+              'text': 'Cupidatat labore est ipsum nostrud'
+            },
+            {
+              'type': 'token',
+              'subtype': 'keyword',
+              'text': 'Given'
+            }
+          ]
+        },
+        {
+          'type': 'step',
+          'subtype': 'when',
+          'nodes': [
+            {
+              'type': 'definition',
+              'text': 'Irure sint sit id laboris incididunt do voluptate'
+            },
+            {
+              'type': 'token',
+              'subtype': 'keyword',
+              'text': 'When'
+            }
+          ]
+        },
+        {
+          'type': 'step',
+          'subtype': 'then',
+          'nodes': [
+            {
+              'type': 'definition',
+              'text': 'Enim enim sit labore ut veniam'
+            },
+            {
+              'type': 'token',
+              'subtype': 'keyword',
+              'text': 'Then'
+            }
+          ]
+        },
+        {
+          'type': 'step',
+          'subtype': 'and',
+          'nodes': [
+            {
+              'type': 'definition',
+              'text': 'Do laboris ullamco nostrud dolore officia'
+            },
+            {
+              'type': 'token',
+              'subtype': 'keyword',
+              'text': 'And'
+            }
+          ]
+        },
+        {
+          'type': 'step',
+          'subtype': 'and',
+          'nodes': [
+            {
+              'type': 'definition',
+              'text': 'Non adipisicing id sunt do qui enim consequat officia mollit eu labore irure dolor'
+            },
+            {
+              'type': 'token',
+              'subtype': 'keyword',
+              'text': 'And'
+            }
+          ]
+        },
+        {
+          'type': 'step',
+          'subtype': 'but',
+          'nodes': [
+            {
+              'type': 'definition',
+              'text': 'Do dolore amet in ut adipisicing proident'
+            },
+            {
+              'type': 'token',
+              'subtype': 'keyword',
+              'text': 'But'
+            }
+          ]
+        },
+        {
+          'subtype': 'keyword',
+          'text': 'Scenario',
+          'type': 'token'
+        },
+        {
+          'subtype': 'colon',
+          'text': ':',
+          'type': 'token'
+        }
       ]
     })
   })
